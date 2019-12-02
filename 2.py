152f34035d2997a28fac64f8f7bd927b387039f2
Nekkowe! <nekkowe@gmail.com>
2019-12-02 20:57:03 +0100

    Day 2 Cleanup

--- 2.py
+++ 2.py
@@ -1,5 +1,5 @@
 def processIntcode(intcode):
-	"""Processes an Intcode and returns the result."""
+	"""Processes an Intcode and returns the resulting output integer."""
 	memory = intcode[:]
 	i = 0
 	while memory[i] is not 99:
@@ -9,29 +9,32 @@ def processIntcode(intcode):
 		elif memory[i] is 2:
 			memory[memory[i+3]] = memory[memory[i+1]] * memory[memory[i+2]]
 			i += 4
-	return memory
+	return memory[0]
 
 def processIntcodeWithInputs(intcode, noun, verb):
-	"""Processes an Intcode, with given parameters replacing positions 1 (noun) and 2 (verb), and returns the result."""
+	"""Processes an Intcode, with given parameters substituted for positions 1 (noun) and 2 (verb), and returns the resulting output integer."""
 	memory = intcode[:]
 	memory[1] = noun
 	memory[2] = verb
 	return processIntcode(memory)
 
+def parseCommaSeparatedIntegers(string):
+	"""Takes a string of comma-separated integers and returns an integer list."""
+	return list(map(int, string.split(',')))
+
 if __name__ == "__main__":
-	# Part 1
 	with open("2.input", "r") as file:
-		intcode = list(map(int, file.read().split(',')))
-	intcode[1] = 12
-	intcode[2] = 2
-	print("Output of Intcode: ", processIntcode(intcode))
+		intcode = parseCommaSeparatedIntegers(file.read())
+
+	# Part 1
+	print("PART 1 | Output of Intcode: ", processIntcodeWithInputs(intcode, 12, 2))
 
 	# Part 2
 	target_output = 19690720
 
 	for noun in range(100):
 		for verb in range(100):
-			output = processIntcodeWithInputs(intcode, noun, verb)[0]
+			output = processIntcodeWithInputs(intcode, noun, verb)
 			if output == target_output:
-				print("Target output", target_output, "reached with Noun:", noun, "Verb:", verb, "Result:", (100*noun + verb))
-	print("All possible noun/verb combinations searched.")
+				print("PART 2 | Target output", target_output, "reached with Noun:", noun, "Verb:", verb, "Result:", (100*noun + verb))
+	print("PART 2 | All possible noun/verb combinations searched.")

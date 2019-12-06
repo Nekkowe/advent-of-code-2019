186eea3885ebc64edfe13295965d7a988cc531ed
Nekkowe! <nekkowe@gmail.com>
2019-12-07 00:46:48 +0100

    Day 5 Part 2

--- 5.py
+++ 5.py
@@ -7,32 +7,68 @@ def processIntcode(intcode):
 		instruction = memory[i]
 		opcode = instruction % 100
 
-		parameter_modes = [int(x) for x in str(instruction // 100)]
-		parameter_modes.reverse()
-		parameter_modes += [0,0]
+		chunk_size = (
+			1 if opcode in [99] else
+			2 if opcode in [3,4] else
+			3 if opcode in [5,6] else
+			4 if opcode in [1,2,7,8] else
+			0)
+
+		chunk = memory[i : i + chunk_size]
+		operands = getOperands(chunk, memory)
+
+		#print(memory,chunk, operands)
 
 		if opcode is 1:
-			operand_1 = memory[i+1] if parameter_modes[0] else memory[memory[i+1]]
-			operand_2 = memory[i+2] if parameter_modes[1] else memory[memory[i+2]]
-			memory[memory[i+3]] = operand_1 + operand_2
-			i += 4
+			write_position = chunk[3]
+			memory[write_position] = operands[0] + operands[1]
 		elif opcode is 2:
-			operand_1 = memory[i+1] if parameter_modes[0] else memory[memory[i+1]]
-			operand_2 = memory[i+2] if parameter_modes[1] else memory[memory[i+2]]
-			memory[memory[i+3]] = operand_1 * operand_2
-			i += 4
+			write_position = chunk[3]
+			memory[write_position] = operands[0] * operands[1]
 		elif opcode is 3:
-			memory[memory[i+1]] = int(input("Input: "))
-			i += 2
+			write_position = chunk[1]
+			memory[write_position] = int(input("Input: "))
 		elif opcode is 4:
-			operand = memory[i+1] if parameter_modes[0] else memory[memory[i+1]]
-			print("Position",i,"|",operand)
-			i += 2
+			print("Position",i,"|",operands[0])
+		elif opcode is 5:
+			i = operands[1] if operands[0] else (i + chunk_size)
+		elif opcode is 6:
+			i = (i + chunk_size) if operands[0] else operands[1]
+		elif opcode is 7:
+			write_position = chunk[3]
+			memory[write_position] = 1 if operands[0] < operands[1] else 0
+		elif opcode is 8:
+			write_position = chunk[3]
+			memory[write_position] = 1 if operands[0] == operands[1] else 0
 		elif opcode is 99:
 			break
 		else:
 			raise ValueError("opcode not recognized: " + str(opcode))
+		
+		if opcode not in [5,6]:
+			i += chunk_size
+		
 	return memory[0]
+
+def getParameterModes(chunk):
+	"""Returns the list of parameter modes contained in the chunk."""
+	instruction = chunk[0]
+
+	parameter_modes = [int(x) for x in str(instruction // 100)]
+	parameter_modes.reverse()
+	parameter_modes += [0] * (len(chunk) - len(parameter_modes))
+
+	return parameter_modes
+
+def getOperands(chunk, memory):
+	operands = []
+	parameter_modes = getParameterModes(chunk)
+
+	for i in range(1, len(chunk)):
+		operand = chunk[i] if parameter_modes[i-1] else memory[chunk[i]]
+		operands.append(operand)
+	
+	return operands
 
 def processIntcodeWithInputs(intcode, noun, verb):
 	"""Processes an Intcode, with given parameters substituted for positions 1 (noun) and 2 (verb), and returns the resulting output integer."""
@@ -49,4 +85,10 @@ if __name__ == "__main__":
 	with open("input/5.input", "r") as file:
 		intcode = parseCommaSeparatedIntegers(file.read())
 
-print(processIntcode(intcode))
+	print(
+		"1: Air Conditioning Unit (Part 1)",
+		"5: Thermal Radiator Controller (Part 2)",
+		sep="\n",
+	)
+
+	processIntcode(intcode)

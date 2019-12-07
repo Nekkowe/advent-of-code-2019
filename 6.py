45c1da98b9cfe8d5ff78fd24c3511d64b01415da
Nekkowe! <nekkowe@gmail.com>
2019-12-07 06:54:40 +0100

    Day 6 Part 2 + Extra (exact path)

--- 6.py
+++ 6.py
@@ -1,3 +1,24 @@
+def getConnectedObjects(object_id, orbits):
+	forward_lookup = [orbits[object_id] if object_id in orbits.keys() else None]
+	reverse_lookup = [key for key in orbits if (orbits[key] == object_id)]
+	return forward_lookup + reverse_lookup
+
+def generateDjikstraMap(orbits, destination):
+	queue = [destination]
+	djikstra_map = {destination: 0}
+
+	while len(queue):
+		current_object = queue.pop(0)
+		distance = djikstra_map[current_object]
+		connected_objects = getConnectedObjects(current_object, orbits)
+		
+		for object_id in connected_objects:
+			if object_id not in djikstra_map.keys():
+				djikstra_map[object_id] = distance + 1
+				queue.append(object_id)
+	
+	return djikstra_map
+
 if __name__ == "__main__":	
 	orbits = {}
 	total_orbits = 0
@@ -17,3 +38,20 @@ if __name__ == "__main__":
 			current_object = orbits[current_object]
 	
 	print("Part 1 | total of direct and indirect orbits:", total_orbits)
+
+	start = orbits["YOU"]
+	destination = orbits["SAN"]
+	djikstra_map = generateDjikstraMap(orbits, destination)
+
+	print("Part 2 | minimum req. # of orbital transfers:", djikstra_map[start])
+
+	path = [start]
+	while destination not in path:
+		cursor = path[-1]
+		connected_objects = getConnectedObjects(cursor, orbits)
+
+		weighted_connections = [(djikstra_map[object_id], object_id) for object_id in connected_objects]
+		weighted_connections.sort()
+		path.append(weighted_connections[0][1])
+
+	print("Bonus  | exact path:",path)

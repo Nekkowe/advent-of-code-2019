b01206a8e44287ab374b12d6b46f3b556cae1e9a
Nekkowe! <nekkowe@gmail.com>
2019-12-11 00:05:58 +0100

    Day 10 part 2

--- 10.py
+++ 10.py
@@ -63,6 +63,14 @@ def getAngle(p1, p2):
 
 	return round(angle,2)
 
+def getDistance(p1, p2):
+	dx = p2["x"] - p1["x"]
+	dy = p2["y"] - p1["y"]
+	
+	h = math.sqrt(dx**2 + dy**2)
+
+	return h
+
 if __name__ == "__main__":
 	with open("input/10.input", "r") as file:
 		asteroids = []
@@ -91,4 +99,44 @@ if __name__ == "__main__":
 
 	base_fitnesses.sort(key=lambda entry: entry["asteroids"], reverse=True)
 
-	print("Part 1 | most asteroids spotted from a base:", base_fitnesses[0]["asteroids"])
+	ideal_base = base_fitnesses[0]
+
+	print("Part 1 | most asteroids spotted from a base:", ideal_base["asteroids"])
+
+	base = ideal_base["coords"]
+
+	asteroids_by_angle = {}
+	for asteroid in asteroids:
+		if asteroid is not base:
+			
+			angle = getAngle(base, asteroid)
+			distance = getDistance(base, asteroid)
+
+			entry = {
+				"distance": distance,
+				"coords": asteroid
+			}
+
+			if angle in asteroids_by_angle.keys():
+				asteroids_by_angle[angle].append(entry)
+			else:
+				asteroids_by_angle[angle] = [entry]
+	
+	vaporized = 0
+
+	while True:
+
+		for angle in sorted(asteroids_by_angle):
+			asteroids = asteroids_by_angle[angle]
+			
+			if len(asteroids):
+				asteroids.sort(key=lambda asteroid: asteroid["distance"])
+
+				vaporized_asteroid = asteroids[0]
+				asteroids.remove(vaporized_asteroid)
+
+				vaporized += 1
+
+				if vaporized is 200:
+					print("Part 2 | 200th vaporized asteroid:", vaporized_asteroid["coords"])
+					break
